50f7977162426c009b965560ba0c6cdfc07bb160
Hudamuse1 <hudabashir150@gmail.com>
2026-04-12 11:40:14 +0200

    Lagt till inkapsling i Room-klassen med private set.

--- OBP200-RolePlayingGame/Room.cs
+++ OBP200-RolePlayingGame/Room.cs
@@ -2,8 +2,8 @@ namespace OBP200_RolePlayingGame;
 
 public class Room
 {
-    public string Type { get; set; }
-    public string Label { get; set; }
+    public string Type { get; private set; }
+    public string Label { get; private set; }
 
     public Room(string type, string label)
     {
@@ -16,6 +16,5 @@ public class BossRoom : Room
 {
     public BossRoom(string label) : base("boss", label)
     {
-        
     }
 }
